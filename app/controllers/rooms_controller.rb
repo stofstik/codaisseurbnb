e6340d5b455610dbe9adbfa7883c6fd69f1a1fac
stofstik <verstegen.daan@gmail.com>
2016-10-11 14:41:48 +0200

    Added room views

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,4 @@
 class RoomsController < ApplicationController
-class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
 
@@ -43,5 +42,4 @@ class RoomsController < ApplicationController
     def room_params
       params.require(:room).permit(:home_type, :room_type, :accommodate, :bedroom_count, :bathroom_count, :listing_name, :description, :address, :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet, :price, :active)
     end
-end
 end
