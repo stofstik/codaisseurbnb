3384ee4dd946f87a65a1e57eb9c1c54db567bd0a
stofstik <verstegen.daan@gmail.com>
2016-10-11 16:22:12 +0200

    One-to-one association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
+  has_one :profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def has_profile?
+    profile.present?
+  end
+
+  def full_name
+    profile.full_name
+  end
 end
