6e71bf363ddf5dbf4ca86516011c2cb1cb8506b8
stofstik <verstegen.daan@gmail.com>
2016-10-12 10:58:19 +0200

    View spec: navbar

--- spec/views/shared/_navbar.html.erb_spec.rb
+++ spec/views/shared/_navbar.html.erb_spec.rb
@@ -1,24 +1,38 @@
 require 'rails_helper'
 
 describe "shared/_navbar.html.erb" do
-  before { sign_in user }
 
-  context "without profile" do
-    let(:user) { create :user }
+  context "user signed in" do
+    before { sign_in user }
 
-    it "renders email" do
+    context "without profile" do
+      let(:user) { create :user }
+
+      it "renders email" do
+        render
+        expect(rendered).to have_content user.email
+      end
+    end
+
+    context "with profile" do
+      let(:profile) { create :profile }
+      let(:user) { create :user, profile: profile }
+
+      it "renders full name" do
+        render
+        puts user.full_name
+        expect(rendered).to have_content user.full_name
+      end
+    end
+
+  end
+
+  context "user not signed in" do
+    it "shows login and signup" do
       render
-      expect(rendered).to have_content user.email
+      expect(rendered).to have_content "Log In"
+      expect(rendered).to have_content "Sign Up"
     end
   end
 
-  context "with profile" do
-    let(:profile) { create :profile }
-    let(:user) { create :user, profile: profile }
-
-    it "renders full name" do
-      render
-      expect(rendered).to have_content user.full_name
-    end
-  end
 end
