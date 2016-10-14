89386fc0975a6dbf048cc519d0cde2931bca48ff
stofstik <verstegen.daan@gmail.com>
2016-10-14 12:03:48 +0200

    Implemented range overlapping check

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,9 +2,16 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    total_price = get_total_price(booking_params)
-    @booking = current_user.bookings.create(booking_params.merge(total: total_price))
-    redirect_to @booking.room, notice: "Thank you for booking!"
+    checkin, checkout = get_dates(booking_params)
+    room = Room.find(booking_params[:room_id])
+
+    if room.available?(checkin, checkout)
+      total_price = get_total_price(booking_params)
+      @booking = current_user.bookings.create(booking_params.merge(total: total_price))
+      redirect_to @booking.room, notice: "Thank you for booking!"
+    else
+      redirect_to room, notice: "Sorry! This listing is not available during the dates you requested."
+    end
   end
 
   private
@@ -13,7 +20,7 @@ class BookingsController < ApplicationController
     checkin, checkout = get_dates(booking_params)
 
     total_days = (checkout - checkin).to_i
-    booking_params[:price].to_i * total_days
+    @room.price * tooal_days
   end
 
   def get_dates(booking_params)
@@ -29,6 +36,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:starts_at, :ends_at, :price, :room_id)
+    params.require(:booking).permit(:starts_at, :ends_at, :room_id)
   end
 end
